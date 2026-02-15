6a39f9dee6d3a301699623fa1698bf275ffc1876
AtheoJester23 <atheojester@gmail.com>
2026-02-16 01:33:27 +0800

    style(dashboard): fix shop phone view

--- src/pages/CreateSched.tsx
+++ src/pages/CreateSched.tsx
@@ -2,7 +2,7 @@ import CreateSchedForm from "../components/CreateSchedForm";
 
 const CreateSched = () => {
     return (  
-        <div className="h-screen p-10 flex justify-center">
+        <div className="py-3 max-sm:px-2 px-7 flex justify-center">
             <CreateSchedForm/>
         </div>
     );
--- src/pages/Shop.tsx
+++ src/pages/Shop.tsx
@@ -5,7 +5,7 @@ const Shop = () => {
   const loc = useLocation().pathname.replace(/[/]/g, "");
 
   return (
-    <div className={`${loc == "shop" ? "mt-15 max-sm:p-2 p-5" : "max-sm:mt-0 max-sm:p-5 p-5" }`}>
+    <div className={`${loc == "shop" ? "mt-15 max-sm:p-2 p-5" : "max-sm:mt-0 max-sm:p-2 p-5" }`}>
       {loc == "shop" && <h1 className='font-bold text-center text-5xl text-yellow-500 max-sm:text-xl bg-[rgb(11,11,11)] p-5'>Available in the shop</h1>}
       <Products/>
     </div>
